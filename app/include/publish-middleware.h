91dac1db98c5b92f0e7cdb3c8682d242404a39b9
Your Name <you@example.com>
2020-03-22 11:44:04 -0300

    Mockup publish middleware

---
+++ app/include/publish-middleware.h
@@ -0,0 +1,26 @@
+/*
+ * publish-middleware.h
+ *
+ *  Created on: Sep 20, 2019
+ *      Author: Team SIP
+ */
+
+#ifndef PUBLISH_H_
+#define PUBLISH_H_
+
+/**************************************************************************
+ * DEFINITIONS
+ **************************************************************************/
+#define PUBLISH_ID          "publish_id"
+#define MIDDLEWARE_PORT		1883
+#define MIDDLEWARE_HOST		"127.0.0.1"
+#define MIDDLEWARE_TOPIC    "status/network"
+
+#define PUB_QOS 1
+#define SUB_QOS 1
+
+/*************************************************************************
+ * INTERNAL FUNCTIONS
+ *************************************************************************/
+
+#endif
---
+++ app/src/publish-middleware.c
@@ -0,0 +1,70 @@
+/**************************************************************************
+ *
+ * publish-middleware.c
+ *
+ *    Application publish 
+ * 
+ * Copyright 2019
+ *
+ **************************************************************************/
+
+#include <middleware.h>
+#include <publish-middleware.h>
+#include <utils.h>
+#include <pthread.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <jansson.h>
+
+#define THIS_FILE	"publish-middleware.c"
+
+middleware_conn conn = NULL;
+
+int main() {
+
+  int status;
+ 
+
+  conn = middleware_open(PUBLISH_ID, MIDDLEWARE_HOST, MIDDLEWARE_PORT, NULL);
+  if (!conn) {
+    log_error("Invalid connection middleware\n");
+  }
+
+  // First Publish - single string
+  {
+    status = middleware_publish(conn, "status/network", "TESTE PUBLISH");
+    printf("status: %d \n", status);
+  }
+
+  sleep(1);
+
+  // Second Publish - json
+  {
+    char *pchData;  
+    json_t *j_data;
+
+    j_data = json_object();
+    if (j_data == NULL) {
+      return ERROR;
+    }   
+
+    json_object_set_new(j_data, "enabledSystem", json_boolean(true));
+    json_object_set_new(j_data, "age", json_integer(10));
+    json_object_set_new(j_data, "name", json_string("Peter"));
+
+    pchData = json_dumps(j_data, JSON_INDENT(2));
+    json_decref(j_data); 
+
+    if (pchData == NULL) {
+      log_error("Error to create message");
+      return ERROR;
+    }  
+
+    status = middleware_publish(conn, "status/network", pchData);
+    printf("status: %d \n", status);  
+
+    free(pchData);
+  }
+
+  return SUCCESS;
+}
